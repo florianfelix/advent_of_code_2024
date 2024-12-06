12daaa759be79ae6acdabb7c4d837dd2ee9d6549
felix <florianfelixmeyer@gmail.com>
2024-12-06 17:59:47 +0100

    redid day01 part 1

--- day01/src/part1.rs
+++ day01/src/part1.rs
@@ -6,7 +6,27 @@ pub fn process(input: &str) -> miette::Result<String> {
     info!("{} - PART 1", "day01".to_uppercase());
     let lines: Vec<String> = input.lines().map(|s| s.to_owned()).collect();
 
-    // info!("{:#?}", lines);
+    let mut left = vec![];
+    let mut right = vec![];
 
-    Ok("RESULT".into())
+    let _: Vec<_> = lines
+        .iter()
+        .map(|s| s.split_whitespace().collect::<Vec<&str>>())
+        .map(|a| {
+            left.push(a.first().unwrap().parse::<i32>().unwrap());
+            right.push(a.last().unwrap().parse::<i32>().unwrap());
+        })
+        .collect();
+
+    left.sort();
+    right.sort();
+
+    let mut sum: i32 = 0;
+
+    for (l, r) in left.iter().zip(right) {
+        let distance = (r - l).abs();
+        sum += distance;
+    }
+
+    Ok(sum.to_string())
 }
