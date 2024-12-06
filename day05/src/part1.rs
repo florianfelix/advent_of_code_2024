8b661f1f1c1fe9c700cdfa4f915c6c5150760c0c
felix <florianfelixmeyer@gmail.com>
2024-12-06 22:08:20 +0100

    parse rules and updates

--- day05/src/part1.rs
+++ day05/src/part1.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+use itertools::Itertools;
 use tracing::info;
 
 #[tracing::instrument(skip(input, test))]
@@ -6,7 +7,32 @@ pub fn process(input: &str, test: &str) -> miette::Result<String> {
     info!("{} - PART 1", "day05".to_uppercase());
     let lines: Vec<String> = test.lines().map(|s| s.to_owned()).collect();
 
-    info!("{:#?}", lines);
+    let splitlines = lines.split(|l| l.is_empty()).collect_vec();
+
+    let mut rules: Vec<(i32, i32)> = splitlines
+        .first()
+        .unwrap()
+        .iter()
+        .map(|l| {
+            let r = l.split_once("|").unwrap();
+            (r.0.parse::<i32>().unwrap(), r.1.parse::<i32>().unwrap())
+        })
+        .collect();
+
+    let mut updates: Vec<Vec<i32>> = splitlines
+        .last()
+        .unwrap()
+        .iter()
+        .map(|l| {
+            let r = l
+                .split(",")
+                .map(|e| e.parse::<i32>().unwrap())
+                .collect_vec();
+            r
+        })
+        .collect();
+
+    info!("{:?}", rules);
 
     Ok("RESULT".into())
 }
