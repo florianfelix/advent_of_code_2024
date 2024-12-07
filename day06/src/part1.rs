571f6544119ebf79e8b6317b42e46e33cbb2f8f8
felix <florianfelixmeyer@gmail.com>
2024-12-07 18:57:35 +0100

    finish day 06 part 1

--- day06/src/part1.rs
+++ day06/src/part1.rs
@@ -15,8 +15,8 @@ enum Facing {
 
 #[derive(Default, Debug)]
 struct Guard {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
     facing: Facing,
 }
 
@@ -24,7 +24,8 @@ struct Guard {
 pub fn process(input: &str, test: &str) -> miette::Result<String> {
     info!("{} - PART 1", "day06".to_uppercase());
     // let lines: Vec<String> = test.lines().map(|s| s.to_owned()).collect();
-    let lines: Vec<&str> = test.lines().collect_vec();
+    let lines: Vec<&str> = input.lines().collect_vec();
+    // let lines: Vec<&str> = test.lines().collect_vec();
 
     let mut guard = Guard::default();
 
@@ -36,8 +37,8 @@ pub fn process(input: &str, test: &str) -> miette::Result<String> {
                 .enumerate()
                 .map(|(x, c)| {
                     if c == '^' {
-                        guard.x = x;
-                        guard.y = y;
+                        guard.x = x as i32;
+                        guard.y = y as i32;
                     };
                     if c == '#' {
                         true
@@ -52,11 +53,75 @@ pub fn process(input: &str, test: &str) -> miette::Result<String> {
     let map: Array2D<bool> = Array2D::from_rows(&lines).into_diagnostic()?;
 
     // unvisited are 0 visted are 1
-    let visited: Array2D<i32> = Array2D::filled_with(0, map.num_rows(), map.num_columns());
+    let mut visited: Array2D<i32> = Array2D::filled_with(0, map.num_rows(), map.num_columns());
 
-    info!("{:?}", guard);
+    // guard.set(0, 4);
+    // info!("{:?}", guard);
+    loop {
+        visited.set(guard.y as usize, guard.x as usize, 1);
+        let next = guard.next_cell();
+        // info!("Next: {:?}", next);
+
+        let out_of_bounds = Guard::out_of_bounds(&map, next);
+
+        if out_of_bounds {
+            // println!("{:#?}", "Guard out of bounds");
+            break;
+        }
+
+        if guard.path_obstructed(&map) {
+            // info!("{:?}", "PATH OBSTRUCTED");
+            guard.turn();
+            // info!("{:?}", guard);
+        } else {
+            guard.set(next.0, next.1);
+            // visited.set(guard.y as usize, guard.x as usize, 1);
+            // info!("{:?}", guard);
+        }
+    }
 
     let count_visited_positions: i32 = visited.elements_row_major_iter().sum();
 
     Ok(count_visited_positions.to_string())
+}
+
+impl Guard {
+    fn set(&mut self, x: impl Into<i32>, y: impl Into<i32>) {
+        self.x = x.into();
+        self.y = y.into();
+    }
+    fn turn(&mut self) {
+        self.facing = match self.facing {
+            Facing::Up => Facing::Right,
+            Facing::Right => Facing::Down,
+            Facing::Down => Facing::Left,
+            Facing::Left => Facing::Up,
+        }
+    }
+    fn next_cell(&self) -> (i32, i32) {
+        match self.facing {
+            Facing::Up => (self.x, self.y - 1),
+            Facing::Right => (self.x + 1, self.y),
+            Facing::Down => (self.x, self.y + 1),
+            Facing::Left => (self.x - 1, self.y),
+        }
+    }
+    fn out_of_bounds(map: &Array2D<bool>, coords: (i32, i32)) -> bool {
+        let (x, y) = coords;
+        if x < 0 {
+            return true;
+        } else if x >= map.num_columns() as i32 {
+            return true;
+        } else if y < 0 {
+            return true;
+        } else if y >= map.num_rows() as i32 {
+            return true;
+        }
+        false
+    }
+    fn path_obstructed(&self, map: &Array2D<bool>) -> bool {
+        let next = self.next_cell();
+        let obstructed = map.get(next.1 as usize, next.0 as usize).unwrap();
+        obstructed.to_owned()
+    }
 }
